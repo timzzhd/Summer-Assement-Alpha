97c0a43483a8db8659909ced409fa26017c80a25
timzzhd <74539817+timzzhd@users.noreply.github.com>
2021-08-14 00:55:43 +0100

    new scene

--- Summer Assessment/Assets/Scripts/PlayerController.cs
+++ Summer Assessment/Assets/Scripts/PlayerController.cs
@@ -12,16 +12,18 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
 
-    public float speed = 20;
-    public float turnSpeed;
-    public float horizontalInput;
-
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
 
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
-
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
+        forwardInput = Input.GetAxis("Vertical");
+        // Moves the car forward based on vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Rotates the car based on horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
